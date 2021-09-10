fd1316b90775aed5c27d53d641bbbdd7b599d513
Pillber <melcard16@gmail.com>
2021-09-09 20:38:33 -0500

    started player creation and networking

--- gamestate.gd
+++ gamestate.gd
@@ -8,7 +8,7 @@ const MAX_PEERS = 12
 var peer: NetworkedMultiplayerENet = null
 
 var players = {}
-var player_name = "Jonathan"
+var player_name = "yo mama"
 var players_ready = []
 
 
@@ -31,12 +31,17 @@ func host_game(new_player_name):
 	peer = NetworkedMultiplayerENet.new()
 	peer.create_server(DEFAULT_PORT, MAX_PEERS)
 	get_tree().set_network_peer(peer)
+	register_self()
 
 func join_game(ip, new_player_name):
 	player_name = new_player_name
 	peer = NetworkedMultiplayerENet.new()
 	peer.create_client(ip, DEFAULT_PORT)
 	get_tree().set_network_peer(peer)
+	register_self()
+	
+func register_self():
+	players[get_tree().get_network_unique_id()] = player_name
 	
 # This is called on all clients (even listen server) whenever someone connects
 func _on_player_connected(id):
@@ -69,6 +74,9 @@ func get_player_list():
 	return players.values()
 
 func start_game():
+		
+	print(players)
+	
 	#Assert means that if this client is not the host --> Stop the code
 	#This means only the host should be running this code (This is a SERVER ONLY COMMAND)
 	assert(get_tree().is_network_server())
@@ -83,12 +91,18 @@ func start_game():
 
 #Run after start button is pressed but before it actually starts
 remote func pre_start_game():
-	
+
 	# Change to game scene while keeping lobby scene loading
 	var Game = load("res://Game.tscn").instance()
 	get_tree().get_root().add_child(Game)
 	get_tree().get_root().get_node("MainMenu").hide()
-
+	
+	for p in players:
+		var player = load("res://Player.tscn").instance()
+		player.set_name(str(p))
+		player.set_network_master(p)
+		get_node("/root/Game").add_child(player) 
+	
 	#If NOT THE SERVER HOST --> Tell server we are ready
 	if not get_tree().is_network_server():
 		# Tell server we are ready to start.
