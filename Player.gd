85a1663780b0fc466bff5db82afb688cee8532f8
Pillber <melcard16@gmail.com>
2021-09-10 10:48:27 -0500

    players appear on screen

--- Player.gd
+++ Player.gd
@@ -3,22 +3,32 @@ extends KinematicBody2D
 export var move_speed = 100
 
 func _ready():
-	$Name.text = gamestate.player_name
 	position = Vector2(100, 100)
+	if gamestate.players.has(name):
+		$Name.text = gamestate.players[name]
+	else:
+		$Name.text = gamestate.player_name
+		
 
 
 func _physics_process(delta):
-	var input = Vector2()
 	
-	if Input.is_action_pressed("move_up"):
-		input += Vector2.UP
-	if Input.is_action_pressed("move_down"):
-		input += Vector2.DOWN
-	if Input.is_action_pressed("move_right"):
-		input += Vector2.RIGHT
-	if Input.is_action_pressed("move_left"):
-		input += Vector2.LEFT
 	
-	input = input.normalized()
 	
-	move_and_slide(input * move_speed)
+	if self.is_network_master():
+		var input = Vector2()
+		
+		if Input.is_action_pressed("move_up"):
+			input += Vector2.UP
+		if Input.is_action_pressed("move_down"):
+			input += Vector2.DOWN
+		if Input.is_action_pressed("move_right"):
+			input += Vector2.RIGHT
+		if Input.is_action_pressed("move_left"):
+			input += Vector2.LEFT
+		
+		input = input.normalized()
+		
+		move_and_slide(input * move_speed)
+	else:
+		position = Vector2(100, 100)
