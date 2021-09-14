9aceac189e933266e94f9938856d28dc7d5717f3
MrBubb1es <lance04preston@gmail.com>
2021-09-14 14:52:40 -0500

    Added Barbed Wire

---
+++ BarbedWire.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+puppet var puppet_pos = Vector2()
+
+func _ready():
+	pass
+
+
+func sync_position():
+	pass
+	
+func _process(delta):
+	if is_network_master():
+		rset("puppet_pos", position)
+	else:
+		position = puppet_pos
