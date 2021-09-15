75a9093a43a5866ab130309d87702205220f6cf7
Wowe-Peanut <47678647+Wowe-Peanut@users.noreply.github.com>
2021-09-15 17:23:11 -0500

    Barbed Wire Slows Player

--- BarbedWire.gd
+++ BarbedWire.gd
@@ -11,3 +11,11 @@ func _process(delta):
 		rset("puppet_pos", position)
 	else:
 		position = puppet_pos
+
+
+func _on_BarbedWire_body_entered(body):
+	body.move_speed /= 2
+
+
+func _on_BarbedWire_body_exited(body):
+	body.move_speed *= 2
