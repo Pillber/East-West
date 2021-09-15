5c9350f805ed4b97ef7d185aff201c3c729ac514
Pillber <melcard16@gmail.com>
2021-09-15 14:24:01 -0500

    ui click sounds added to main menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,20 +1,23 @@
 extends VBoxContainer
 
-signal play_pressed()
+onready var button_sound = $ButtonSound
 
-func _ready():
-	$ThemeMusic.play()
+signal play_pressed()
 
 func _on_PlayButton_pressed():
 	print("playing...")
 	$ThemeMusic.stop()
+	button_sound.play()
 	emit_signal("play_pressed")
 
 
 func _on_OptionsButton_pressed():
 	print("options...")
+	button_sound.play()
 
 
 func _on_QuitButton_pressed():
 	print("quiting...")
+	button_sound.play()
+	yield(button_sound, "finished")
 	get_tree().quit()
